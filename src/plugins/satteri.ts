a304a0532eb128283913e53eb1a39a4ae7eabfae
abeminus <abeminus@protonmail.com>
2026-08-30 16:44:30 +0900

    satteri issues

--- src/plugins/satteri.ts
+++ src/plugins/satteri.ts
@@ -46,7 +46,7 @@ export function satteriReadingTimePlugin(): () => MdastPluginDefinition {
 				const textOnPage = mdastToString(root);
 				const readingTime = getReadingTime(textOnPage);
 
-				ctx.data.astro!.frontmatter.readingTime = readingTime.text;
+				(ctx.data as any).astro!.frontmatter.readingTime = readingTime.text;
 			},
 		};
 	};
