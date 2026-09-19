79f15a39b3044ecd76242c06d04f24129094cf88
abeminus <abeminus@protonmail.com>
2026-09-19 14:01:00 +0900

    remark.ts edits

--- src/utils/remark.ts
+++ src/utils/remark.ts
@@ -1,19 +1,6 @@
 import { h as _h, type Properties } from "hastscript";
 import type { Paragraph } from "mdast";
-import type { Element } from "hast";
 
-const element: Element = {
-  type: "element",
-  tagName,
-  properties,
-  children: [],
-  data: {
-    hName: tagName,
-    hProperties: properties,
-  },
-};
-
-return element;
 /** From Astro Starlight: Function that generates an mdast HTML tree ready for conversion to HTML by rehype. */
 // biome-ignore lint/suspicious/noExplicitAny: allow any children
 export function h(el: string, attrs: Properties = {}, children: any[] = []): Paragraph {
